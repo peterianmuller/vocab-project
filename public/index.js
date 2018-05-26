23ac51a81e5904a97015ed1aca472a9f641c7b1f
peterianmuller <peterianmuller@gmail.com>
2018-05-26 11:32:29 -0700

    PArsing of API paylod working when definition is an array

--- public/index.js
+++ public/index.js
@@ -1,3 +1,33 @@
+const parseResponse = resData => {
+	// for every resData we get data.entry_list.entry, which is an array if there are multiple entries, or an object if there is a single entry.
+	// let's write a function to create an item for one word
+	Array.isArray(resData)
+		? resData.forEach(word => {
+				parseOneEntry(word);
+		  })
+		: parseOneEntry(resData);
+};
+
+const parseOneEntry = entry => {
+	// what different data types is entry.def.dt?
+	// either string, array, or object
+	// deal with strings first. Problem here is we're dealing with phrases that contain the word 'chicken-and-egg', etc.
+
+	if (typeof entry.def.dt === 'string') {
+		console.log(entry.def);
+		let div = $('<div></div>');
+		div.append(document.createTextNode(`${entry.id} ${entry.def.dt}`));
+		$('.words-container').append(div);
+	} else if (Array.isArray(entry.def.dt)) {
+		entry.def.dt.forEach(definition => {
+			let div = $('<div></div>');
+			console.log(definition, entry.id);
+			div.append(document.createTextNode(`${entry.id} ${definition}`));
+			$('.words-container').append(div);
+		});
+	}
+};
+
 $('button:first-of-type').click(e => {
 	let word = $(`input[name*='word']`).val();
 	$(`input[name*='word']`).val('loading...');
@@ -9,16 +39,10 @@ $('button:first-of-type').click(e => {
 		},
 		url: 'lookup',
 		dataType: 'json',
-		// headers: {
-		// 	'Access-Control-Allow-Origin': '*',
-		// 	'Content-Type': 'application/json;charset=UTF-8',
-		// 	'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-		// 	'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
-		// },
 		contentType: 'application/json',
 		success: data => {
 			$(`input[name*='word']`).val($(`input[name*='word']`).attr('placeholder'));
-			console.log(data);
+			parseResponse(data.entry_list.entry);
 		}
 	});
 });
--- server.js
+++ server.js
@@ -5,26 +5,21 @@ var axios = require('axios');
 var parser = require('xml2json');
 
 app.use(bodyParser.json());
-
-let key = require('./config').keyWebsterSpanish;
-console.log(key);
-
 app.use(express.static(`${__dirname}/public`));
 
-var teams = [];
+let keySpan = require('./config').keyWebsterSpanish;
+let keyEng = require('./config').keyWebsterEng;
 
 app.get('/lookup', (req, res) => {
 	console.log(req);
 	console.log('right route');
 	axios
-		.get(`https://www.dictionaryapi.com/api/v1/references/spanish/xml/${req.query.word}?key=${key}`)
+		.get(`https://www.dictionaryapi.com/api/v1/references/collegiate/xml/${req.query.word}?key=${keyEng}`)
 		.catch(function(error) {
 			console.log(`error is: ${error}`);
 		})
 		.then(response => {
 			var json = parser.toJson(response.data);
-			console.log(json);
-			//res.send(response);
 			res.send(json);
 		});
 });
