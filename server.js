58e26d7f2012a44f521768d7b9171818b030d24a
peterianmuller <peterianmuller@gmail.com>
2019-08-01 12:07:14 -0700

    Add back link to get to the previous page

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var request = require('request');
 let oxford = require('oxford-dictionaries-api');
 
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/public`));
 
@@ -20,29 +20,37 @@ let oxforddictionaries = new oxford(appId, appKey);
 app.get('/lookup', (req, res) => {
 	console.log('req.query.word is:', req.query.word);
 	oxforddictionaries
-		.entries({ word_id: req.query.word })
+		.entries({ word_id: req.query.word, filter: 'fields=definitions' })
 		.then(data => {
-			//console.log(data);
+			// console.log(data);
+			// create a new data set that shows the word
+			let wordInfo = {};
+			//console.log('data is:', data);
 			data.results.forEach(datum => {
+				// from here I can grab the language
+				//console.log('datum is:', datum);
+				wordInfo.language = datum.language;
 				datum.lexicalEntries.forEach(entry => {
 					entry.entries.forEach(example => {
-						//console.log(example.senses);
+						// console.log('example is: ', example);
 						example.senses.forEach(sense => {
-							console.log('sense is: ', sense);
+							//	if (!sense.subsenses) console.log('sense is: ', sense);
 							if (sense.subsenses) {
 								sense.subsenses.forEach(subsense => {
-									console.log('sense is:', sense);
-									console.log('subsense is:', subsense);
+									//	console.log('sense is:', sense);
+									//	console.log('subsense is:', subsense);
 								});
 							}
 						});
 					});
 				});
 			});
-			res.render('results', { word: data });
+
+			res.render('results', { word: data, wordInfo: wordInfo });
 		})
 		.catch(error => {
 			console.log(`error is: ${error}`);
+			res.redirect('/');
 		});
 });
 
